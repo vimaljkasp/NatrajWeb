92e9bd67689ed29a6a69d805ffeed8649378df71
aniljks <aniljks@gmail.com>
2019-04-07 14:50:02 +0530

    Minor issue fix for VLC payment report

--- Platform.DTO/Interfaces/Reports/IDockCollectionReportService.cs
+++ Platform.DTO/Interfaces/Reports/IDockCollectionReportService.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace Platform.DTO
 {
-   public  interface IDockCollectionReportService
+    public interface IDockCollectionReportService
     {
-         ResponseDTO DockCollectionSummaryByDate(DateTime collectionStartDate, DateTime collectionEndDate, int startShift, int endShift, int milkType);
+        ResponseDTO DockCollectionSummaryByDate(DateTime collectionStartDate, DateTime collectionEndDate, int startShift, int endShift, int milkType);
 
         ResponseDTO DockCollectionSummaryByVLC(int vlcId, DateTime collectionStartDate, DateTime collectionEndDate, int startShift, int endShift, int milkType);
 
-   //     ResponseDTO DockCollectionSummaryDetailByVLC(int vlcId, DateTime collectionStartDate, DateTime collectionEndDate);
+        //ResponseDTO DockCollectionSummaryDetailByVLC(int vlcId, DateTime collectionStartDate, DateTime collectionEndDate);
 
 
 
